1cb0a9f7b1ec29b1f2eb4b258a0cf904e6f98a37
flutterone <50611598+flutterone@users.noreply.github.com>
2019-06-30 01:37:27 +0300

    Update decorator.dart

--- decorator.dart
+++ decorator.dart
@@ -1,80 +1,140 @@
 void main() {
-  Beverage beverage = Americano();
-  print('${beverage.getDescription()} ${beverage.cost()}\$');
+  Beverage beverage = Americano(Size.Big);
+  print('${beverage.getDescription()} ${beverage.cost().toStringAsFixed(2)}\$');
   beverage = Mocha(beverage);
   beverage = Soy(beverage);
   beverage = Whip(beverage);
-  print('${beverage.getDescription()} ${beverage.cost()}\$');
+  print('${beverage.getDescription()} ${beverage.cost().toStringAsFixed(2)}\$');
 }
 
+enum Size { Litle, Average, Big }
+
 abstract class Beverage {
+  Size size;
   String description = '';
   String getDescription() {
     return description;
   }
 
+  void setSize(Size size) {
+    this.size = size;
+  }
+
+  Size getSize() => size;
   double cost();
 }
 
-abstract class CondimentDecorator extends Beverage {
-  String getDescription();
-}
+abstract class CondimentDecorator extends Beverage {}
 
 class Espresso extends Beverage {
-  Espresso() {
-    description = 'Espresso';
+  Espresso(Size size) {
+    description =
+        'Espresso ${size.toString().toLowerCase().replaceAll('size.', '')}';
+    setSize(size);
   }
   @override
   double cost() {
-    return 1.99;
+    double price;
+    if (size == Size.Litle) {
+      price = 1.99;
+    } else if (size == Size.Average) {
+      price = 2.30;
+    } else if (size == Size.Big) {
+      price = 2.60;
+    }
+    return price;
   }
 }
 
 class Americano extends Beverage {
-  Americano() {
-    description = 'Americano';
+  Americano(Size size) {
+    description =
+        'Americano ${size.toString().toLowerCase().replaceAll('size.', '')}';
+    setSize(size);
   }
   @override
   double cost() {
-    return 0.99;
+    double price;
+    if (size == Size.Litle) {
+      price = 0.99;
+    } else if (size == Size.Average) {
+      price = 1.30;
+    } else if (size == Size.Big) {
+      price = 1.60;
+    }
+    return price;
   }
 }
 
 class Mocha extends CondimentDecorator {
-  final Beverage _beverage;
-  Mocha(this._beverage);
+  final Beverage beverage;
+  Mocha(this.beverage) {
+    setSize(beverage.size);
+  }
+
+  @override
   String getDescription() {
-    return '${_beverage.getDescription()}, Mocha';
+    return '${beverage.getDescription()}, Mocha';
   }
 
   @override
   double cost() {
-    return 0.20 + _beverage.cost();
+    double price;
+    if (size == Size.Litle) {
+      price = 0.20;
+    } else if (size == Size.Average) {
+      price = 0.30;
+    } else if (size == Size.Big) {
+      price = 0.40;
+    }
+    return price + beverage.cost();
   }
 }
 
 class Soy extends CondimentDecorator {
-  final Beverage _beverage;
-  Soy(this._beverage);
+  final Beverage beverage;
+  Soy(this.beverage) {
+    setSize(beverage.size);
+  }
+  @override
   String getDescription() {
-    return '${_beverage.getDescription()}, Soy';
+    return '${beverage.getDescription()}, Soy';
   }
 
   @override
   double cost() {
-    return 0.50 + _beverage.cost();
+    double price;
+    if (size == Size.Litle) {
+      price = 0.50;
+    } else if (size == Size.Average) {
+      price = 0.60;
+    } else if (size == Size.Big) {
+      price = 0.70;
+    }
+    return price + beverage.cost();
   }
 }
 
 class Whip extends CondimentDecorator {
-  final Beverage _beverage;
-  Whip(this._beverage);
+  final Beverage beverage;
+  Whip(this.beverage) {
+    setSize(beverage.size);
+  }
+  @override
   String getDescription() {
-    return '${_beverage.getDescription()}, Whip';
+    return '${beverage.getDescription()}, Whip';
   }
 
   @override
   double cost() {
-    return 0.30 + _beverage.cost();
+    double price;
+    if (size == Size.Litle) {
+      price = 0.40;
+    } else if (size == Size.Average) {
+      price = 0.50;
+    } else if (size == Size.Big) {
+      price = 0.60;
+    }
+    return price + beverage.cost();
   }
 }
