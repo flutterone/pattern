e24ebb26455f125fcd03774b0259ca970a656e64
flutterone <50611598+flutterone@users.noreply.github.com>
2019-06-30 01:54:04 +0300

    Update observer.dart

--- observer.dart
+++ observer.dart
@@ -1,114 +1,91 @@
-main() async {
-  WheatherData weatherData = WheatherData();
-  Display display = DisplayDetail();
-  display.register(weatherData);
-  weatherData.setMeasuremensChanged(22, 35, 750);
-  display.dispose();
-  await Future.delayed(Duration(seconds: 3));
-  Display displayTemp = DisplayTemp();
-  displayTemp.register(weatherData);
-  weatherData.setMeasuremensChanged(23, 50, 760);
-  displayTemp.dispose();
+void main() {
+  NewsData newsData = NewsData();
+  Display display = Display();
+  display.registerListenNewsData(newsData);
+  newsData.setNews('Заголовок новости', 'Тело новости');
+  display.removeListenNewsData();
+  newsData.setNews('Заголовок новости 2', 'Тело новости 2');
 }
 
-class WheatherData implements Subject {
-  List<Observer> observers;
-  double temp;
-  double humidity;
-  double pressure;
 
-  WheatherData() {
-    observers = [];
-  }
+
+
+class NewsData implements Subject {
+  List<Observer> observers = [];
+  String _title;
+  String _body;
 
   @override
-  void notyfyObserver() {
-    for (var i = 0; i < observers.length; i++) {
-      Observer observer = observers[i];
-      print('notyfyObserver ${observers[i]}');
-      observer.update(temp, humidity, pressure);
-    }
-  }
-
-  @override
-  void registerObserver(Observer observer) {
+  void _addObserver(Observer observer) {
     observers.add(observer);
-    print('registerObserver $observer');
+    print('add $observer');
   }
 
   @override
-  void removeObserver(Observer observer) {
-    int i = observers.indexOf(observer);
-    if (i >= 0) {
-      observers.removeAt(i);
-      print('removeObserver $observer');
+  void _notyfyObserver() {
+    for (Observer item in observers) {
+      item._update(_title, _body);
     }
   }
 
-  void measurementsChanged() {
-    notyfyObserver();
+  @override
+  void _removeObserver(Observer observer) {
+    bool result = observers.remove(observer);
+    result ? print('$observer is removed') : print('$observer not found');
   }
 
-  void setMeasuremensChanged(double temp, double humidity, double pressure) {
-    this.temp = temp;
-    this.humidity = humidity;
-    this.pressure = pressure;
-    measurementsChanged();
+  void _newsUpdated() {
+    _notyfyObserver();
+  }
+
+  void setNews(String title, String body) {
+    this._title = title;
+    this._body = body;
+    _newsUpdated();
   }
 }
 
 abstract class Subject {
-  void registerObserver(Observer observer);
-  void removeObserver(Observer observer);
-  void notyfyObserver();
+  void _addObserver(Observer observer);
+  void _removeObserver(Observer observer);
+  void _notyfyObserver();
 }
 
 abstract class Observer {
-  void update(double temp, double humidity, double pressure);
+  void _update(String title, String body);
+  void registerListenNewsData(Subject newsData);
+  void removeListenNewsData();
 }
 
 abstract class DisplayElement {
-  void display();
+  void _display();
 }
 
-abstract class Display implements Observer, DisplayElement {
-  double temp;
-  double humidity;
-  double pressure;
-  Subject weatherData;
+class Display implements Observer, DisplayElement {
+  String _title;
+  String _body;
+  Subject _newsData;
 
-  void register(Subject weatherData) {
-    this.weatherData = weatherData;
-    weatherData.registerObserver(this);
-  }
-
-  void dispose() {
-    weatherData.removeObserver(this);
+  @override
+  void _display() {
+    print('$_title $_body');
   }
 
   @override
-  void update(double temp, double humidity, double pressure) {
-    this.temp = temp;
-    this.humidity = humidity;
-    this.pressure = pressure;
-    display();
+  void _update(String title, String body) {
+    this._title = title;
+    this._body = body;
+    _display();
   }
 
   @override
-  void display() {}
-}
-
-class DisplayDetail extends Display {
-  @override
-  void display() {
-    print('$temp, $humidity, $pressure');
-    super.display();
+  void registerListenNewsData(Subject newsData) {
+    this._newsData = newsData;
+    newsData._addObserver(this);
   }
-}
 
-class DisplayTemp extends Display {
   @override
-  void display() {
-    print(temp);
+  void removeListenNewsData() {
+    _newsData._removeObserver(this);
   }
 }
